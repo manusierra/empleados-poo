1bdedbe5bb96e7f2a0270ad5f5efb1d6856ab796
manu.sierra <manu.sierra@outlook.com>
2020-10-22 12:32:09 +0200

    inicio

---
+++ empleados.php
@@ -0,0 +1,19 @@
+<?php
+
+// Cree las siguientes clases:
+
+// - La clase Empleado debe tener un primer nombre, apellido paterno y número de seguro social.
+// - La clase EmpleadoAsalariado (hereda de Empleado) debe tener un salario semanal.
+// - La clase EmpleadoPorHoras (hereda de Empleado) debe tener un sueldo y el número de horas trabajadas.
+// - La clase EmpleadoPorComision (hereda de Empleado) debe tener una tasa de comisiones y ventas brutas.
+// - La clase EmpleadoBaseMasComision (hereda de EmpleadoPorComisión) debe tener un salario base.
+// - Cada clase debe tener los constructores apropiados, los métodos establecer y obtener.
+
+// Escriba un programa que cree instancias de objetos de cada una de estas clases, y que muestre toda la información asociada con cada objeto (incluyendo la información heredada)
+
+// Establecemos la clase padre 'Empleado'.
+
+class Empleado
+{
+
+}
